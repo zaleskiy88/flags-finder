92ff8e5a5df45491f64f8c0ba19a1d750ee08390
zaleskiy88 <zaleskiy88@gmail.com>
2024-03-07 18:31:17 +0000

    updated pagination logic. Now it works as supossed to work :)

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,4 +1,10 @@
-import { PaginationWrapper } from "components/index";
+import { useState } from "react";
+import {
+  PaginationWrapper,
+  PaginationList,
+  PaginationBtn,
+  ActiveButton,
+} from "components/index";
 
 /**
  * Pagination component for navigating through a list of items
@@ -7,44 +13,65 @@ import { PaginationWrapper } from "components/index";
  * @param {number} length - the total number of items in the list
  */
 export const Pagination = ({ index, setIndex, length }) => {
-  /**
-   * Function to navigate to the next item in the list
-   */
+  const [currentBtn, setCurrentBtn] = useState(index + 1);
+  const screenWidth = window.innerWidth;
+  const maxButtons = screenWidth < 768 ? 6 : length; // Set the maximum number of buttons to display
+  const halfMaxButtons =
+    // Calculates the value of halfMaxButtons based on the condition of the screenWidth.
+    // If the screenWidth is less than 768, it assigns the result of Math.floor(maxButtons / 2) to halfMaxButtons,
+    // otherwise it assigns the value of length to halfMaxButtons.
+    screenWidth < 768 ? Math.floor(maxButtons / 2) : length;
+  //
+  const start =
+    //calculates the starting index for a range, ensuring that it's at least 0.
+    Math.max(0, index - halfMaxButtons);
+  //
+  const end = screenWidth < 768 ? Math.min(length, start + maxButtons) : length;
+
   const goForward = () => {
+    /** Function to navigate to the next item in the list **/
     if (index >= length - 1) {
       return;
     }
     setIndex(index + 1);
+    setCurrentBtn(currentBtn + 1);
   };
 
-  /**
-   * Function to navigate to the previous item in the list
-   */
   const goBack = () => {
+    /**  Function to navigate to the previous item in the list  */
     if (index <= 0) {
       return;
     }
     setIndex(index - 1);
+    setCurrentBtn(currentBtn - 1);
   };
 
-  /**
-   * Function to navigate to a specific page in the list
-   * @param {number} page - the index of the page to navigate to
-   */
   const goToPage = (page) => {
+    /**
+     * Function to navigate to a specific page in the list
+     * @param {number} page - the index of the page to navigate to
+     */
     setIndex(page);
+    setCurrentBtn(page + 1);
   };
+  //
 
   return (
     <PaginationWrapper>
       <button onClick={goBack}>prev</button>
-      <ul>
-        {Array.from({ length: length }, (_, index) => (
-          <button key={index} onClick={() => goToPage(index)}>
-            {index + 1}
-          </button>
-        ))}
-      </ul>
+      <PaginationList>
+        {Array.from({ length: end - start }, (_, i) =>
+          start + i === index ? (
+            <ActiveButton key={start + i} onClick={() => goToPage(start + i)}>
+              {start + i + 1}
+            </ActiveButton>
+          ) : (
+            <PaginationBtn key={start + i} onClick={() => goToPage(start + i)}>
+              {start + i + 1}
+            </PaginationBtn>
+          )
+        )}
+      </PaginationList>
       <button onClick={goForward}>next</button>
     </PaginationWrapper>
   );
