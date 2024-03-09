fb26dca48ccd8c507e4ec32bb0c9f4ec621e0f47
zaleskiy88 <zaleskiy88@gmail.com>
2024-03-09 19:26:36 +0000

    updated grid sizes of the flag´s list

--- src/components/countriesList/CountriesList.styled.js
+++ src/components/countriesList/CountriesList.styled.js
@@ -6,24 +6,25 @@ export const Section = styled.section`
   padding-bottom: 65px;
 
   display: grid;
-  grid-template-columns: repeat(1, 1fr);
+  grid-template-columns: repeat(1, minmax(300px, 336px));
+  justify-content: center;
   grid-gap: 40px;
 
   @media screen and (min-width: 767px) {
     padding-top: 48px;
     padding-bottom: 48px;
 
-    grid-template-columns: repeat(2, 1fr);
+    grid-template-columns: repeat(2, minmax(300px, 336px));
     grid-gap: 55px;
   }
 
   @media screen and (min-width: 1024px) {
     grid-template-columns: repeat(3, 1fr);
-    grid-gap: 75px;
+    grid-gap: 74px;
   }
 
   @media screen and (min-width: 1280px) {
-    grid-template-columns: repeat(4, 1fr);
-    grid-gap: 75px;
+    grid-template-columns: repeat(4, minmax(250px, 336px));
+    grid-gap: 74px;
   }
 `;
